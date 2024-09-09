4860acea64cd3d37cf2c0d0a382d96987b67465a
rafaelnmo <rafaelnevesmello@gmail.com>
2024-09-09 14:12:56 -0300

    Application test to run the comunication library

--- include/application.h
+++ include/application.h
@@ -1,10 +1,15 @@
 #ifndef APPLICATION_H
 #define APPLICATION_H
 
+#include "reliable_comm.h"
+
 class Application {
 public:
-    Application();
+    Application(ReliableComm* comm);
+    void run();
 
+private:
+    ReliableComm* comm;
 };
 
 #endif
--- src/application.cpp
+++ src/application.cpp
@@ -1,8 +1,24 @@
 #include "application.h"
+#include <iostream>
 
-Application::Application(){
+Application::Application(ReliableComm* comm)
+    : comm(comm) {}
 
-    // TO DO
+void Application::run() {
+    // Example usage
+    std::vector<uint8_t> message = { 'H', 'e', 'l', 'l', 'o' };
 
+    // Send a message to process 1
+    comm->send(1, message);
+
+    // Broadcast a message to all processes
+    comm->broadcast(message);
+
+    // Wait to receive a message
+    Message received = comm->receive();
+    std::cout << "Received message from process " << received.sender_id << ": ";
+    for (auto byte : received.content) {
+        std::cout << byte;
+    }
+    std::cout << std::endl;
 }
-
