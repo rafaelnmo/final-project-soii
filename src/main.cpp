21acd17413467cf7099218117455ce6e277a4111
rafaelnmo <rafaelnevesmello@gmail.com>
2024-09-09 14:13:44 -0300

    Main claas to setup the environment included

--- src/main.cpp
+++ src/main.cpp
@@ -5,8 +5,19 @@
 #include <iostream>
 
 int main() {
+    std::map<int, std::pair<std::string, int>> nodes = {
+        {0, {"127.0.0.1", 3000}},
+        {1, {"127.0.0.1", 3001}},
+        {2, {"127.0.0.1", 3002}}
+    };
 
-    // TO DO
+    Channels channels(nodes);
+    channels.bind_socket(0);
+
+    ReliableComm comm(0, nodes);
+    Application app(&comm);
+
+    app.run();
 
     return 0;
 }
