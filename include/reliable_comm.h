3fdb117440fdb4f91877212b9bc22b6c331bf8d3
rafaelnmo <rafaelnevesmello@gmail.com>
2024-09-09 14:11:49 -0300

    Comumication protocol class with basic methods included

--- include/reliable_comm.h
+++ include/reliable_comm.h
@@ -1,10 +1,40 @@
 #ifndef RELIABLE_COMM_H
 #define RELIABLE_COMM_H
 
+#include "message.h"
+#include <map>
+#include <set>
+#include <queue>
+#include <mutex>
+#include <condition_variable>
+
+class Channels;
+class FailureDetection;
+
 class ReliableComm {
 public:
-    ReliableComm();
+    ReliableComm(int id, const std::map<int, std::pair<std::string, int>>& nodes);
+    void send(int id, const std::vector<uint8_t>& message);
+    void broadcast(const std::vector<uint8_t>& message);
+    Message receive();
+    Message deliver();
 
+private:
+    int process_id;
+    std::map<int, std::pair<std::string, int>> nodes;
+    std::set<int> delivered_messages;
+    
+    Channels* channels;
+    FailureDetection* failure_detection;
+    
+    std::mutex mtx;
+    std::condition_variable cv;
+    std::queue<Message> message_queue;
+
+    void listen();
+    void send_message(int id, const std::vector<uint8_t>& message);
+    bool is_delivered(int msg_hash);
+    void mark_delivered(int msg_hash);
 };
 
 #endif
--- src/reliable_comm.cpp
+++ src/reliable_comm.cpp
@@ -1,9 +1,72 @@
 #include "reliable_comm.h"
+#include "channels.h"
+#include "failure_detection.h"
+#include <cstring>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <unistd.h>
+#include <thread>
+#include <iostream>
 
 #define MAX_BUFFER_SIZE 1024
 
-ReliableComm::ReliableComm(){
+ReliableComm::ReliableComm(int id, const std::map<int, std::pair<std::string, int>>& nodes)
+    : process_id(id), nodes(nodes) {
+    // Initialize Channels and FailureDetection
+    channels = new Channels(nodes);
+    failure_detection = new FailureDetection();
 
-    // TO DO
-    
+    // Create listener thread
+    std::thread listener(&ReliableComm::listen, this);
+    listener.detach();
+}
+
+void ReliableComm::send(int id, const std::vector<uint8_t>& message) {
+    send_message(id, message);
+}
+
+void ReliableComm::broadcast(const std::vector<uint8_t>& message) {
+    for (const auto& node : nodes) {
+        if (node.first != process_id) {
+            send_message(node.first, message);
+        }
+    }
+}
+
+void ReliableComm::send_message(int id, const std::vector<uint8_t>& message) {
+    channels->send_message(id, message);
+}
+
+bool ReliableComm::is_delivered(int msg_hash) {
+    return delivered_messages.find(msg_hash) != delivered_messages.end();
+}
+
+void ReliableComm::mark_delivered(int msg_hash) {
+    delivered_messages.insert(msg_hash);
+}
+
+void ReliableComm::listen() {
+    while (true) {
+        auto [msg, msg_hash] = channels->receive_message();
+        
+        std::unique_lock<std::mutex> lock(mtx);
+        if (!is_delivered(msg_hash)) {
+            mark_delivered(msg_hash);
+            message_queue.push(msg);
+            cv.notify_all();
+        }
+    }
+}
+
+Message ReliableComm::receive() {
+    std::unique_lock<std::mutex> lock(mtx);
+    cv.wait(lock, [this] { return !message_queue.empty(); });
+
+    Message msg = message_queue.front();
+    message_queue.pop();
+    return msg;
+}
+
+Message ReliableComm::deliver() {
+    return receive();
 }
