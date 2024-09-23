e776d2206199ae73a7a594d25713359452088dc5
Alexis Mendes Sequeira <alexisora@gmail.com>
2024-09-23 17:08:59 -0300

    adicionando comentários adicionais

--- src/failure_detection.cpp
+++ src/failure_detection.cpp
@@ -17,10 +17,14 @@ bool FailureDetection::timedout() {
 
 /*
     ==EXEMPLO DE USO==
-    message = std::pair<Message, int> (Message(0, {}), 0)
+    message = std::pair<Message, int> (Message(0, {}), 0) troca aqui pela mensagem de controle e o == por != para início/final da comunicação
     fdetec = new FailureDetection(10000);
     fdetec.starttime();
     while(!fdetec.timedout() && message==(Message(0, {}), 0){
         message = receive_message();
     }
+
+    outra ideia seria iniciar start no construtor
+    vantagem: não precisa rodar starttime(), código mais enxuto
+    desvantagem: precisaria instanciar um novo FailureDetection antes de cada envio/recebimento
 */
