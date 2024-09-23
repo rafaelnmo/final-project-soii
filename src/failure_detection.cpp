a9107926fdf013f6883966c4456440cdc49c9552
Alexis Mendes Sequeira <alexisora@gmail.com>
2024-09-23 16:13:21 -0300

    implementação inicial

--- src/failure_detection.cpp
+++ src/failure_detection.cpp
@@ -1,7 +1,26 @@
 #include "failure_detection.h"
+using namespace std::chrono;
 
-FailureDetection::FailureDetection() {
-    
-    // TO DO
-    
+FailureDetection::FailureDetection(double timeout) {
+    max_time = timeout;
 }
+
+void FailureDetection::starttime() {
+    start = high_resolution_clock::now();
+}
+
+bool FailureDetection::timedout() {
+    double now = high_resolution_clock::now();
+    double duration = duration_cast<milliseconds>(now - start);
+    return (duration>max_time);
+}
+
+/*
+    ==EXEMPLO DE USO==
+    message = std::pair<Message, int> (Message(0, {}), 0)
+    fdetec = new FailureDetection(10000);
+    fdetec.starttime();
+    while(!fdetec.timedout() && message==(Message(0, {}), 0){
+        message = receive_message();
+    }
+*/
