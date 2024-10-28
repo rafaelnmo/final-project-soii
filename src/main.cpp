13dfb8a69642f9ce46e36de468f2ca80b58c45ff
rafaelnmo <rafaelnevesmello@gmail.com>
2024-10-27 23:30:18 -0300

    Updating main to include atomicBroadcast selection

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include "application.h"
 #include "reliable_comm.h"
 #include "channels.h"
+#include "atomic_broadcast_ring.h"
 #include <iostream>
 
 int main(int argc, char** argv) {
@@ -10,14 +11,22 @@ int main(int argc, char** argv) {
         {2, {"127.0.0.1", 3002}}
     };
 
-    std::string broadcast_type = "UR";
+    //std::string broadcast_type = "UR";
     // Channels channels(nodes);
     // channels.bind_socket(atoi(argv[1]));
 
+    std::string broadcast_type;
+    std::cout << "Escolha o tipo de broadcast ('BE', 'UR,' ou 'AB'): ";
+    std::cin >> broadcast_type;
+
+    //ReliableComm comm(atoi(argv[1]), nodes, broadcast_type);
+    //Application app(&comm);
+
     ReliableComm comm(atoi(argv[1]), nodes, broadcast_type);
-    Application app(&comm);
+    AtomicBroadcastRing atomic_broadcast(atoi(argv[1]), &comm, { {0, 1}, {1, 2}, {2, 0} });
 
-    app.run(atoi(argv[1]));
-
+    Application app(&comm, &atomic_broadcast);
+    app.run(atoi(argv[1]), broadcast_type);
+    
     return 0;
 }
