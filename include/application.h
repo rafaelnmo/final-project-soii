3ea9170d7c619e82585ec63b858cf6e38972c581
rafaelnmo <rafaelnevesmello@gmail.com>
2024-10-28 19:22:18 -0300

    Simplest version of apppication with AB inheriting from Reliable comm

--- include/application.h
+++ include/application.h
@@ -2,20 +2,14 @@
 #define APPLICATION_H
 
 #include "reliable_comm.h"
-#include "atomic_broadcast_ring.h"
-#include <iostream>
 
 class Application {
 public:
-    Application(ReliableComm* comm, AtomicBroadcastRing* atomic_broadcast);
-    void run(int process_id, const std::string& broadcast_type);
+    Application(ReliableComm* comm);
+    void run(int process_id);
 
 private:
     ReliableComm* comm;
-    AtomicBroadcastRing* atomic_broadcast;
-
-    void reliable_broadcast_run(int process_id);
-    void atomic_broadcast_run(int process_id);
 };
 
 #endif
--- src/application.cpp
+++ src/application.cpp
@@ -1,56 +1,29 @@
 #include "application.h"
 #include <iostream>
 
-Application::Application(ReliableComm* comm, AtomicBroadcastRing* atomic_broadcast)
-    : comm(comm), atomic_broadcast(atomic_broadcast) {}
+Application::Application(ReliableComm* comm)
+    : comm(comm) {}
 
-void Application::run(int process_id, const std::string& broadcast_type) {
-    if (broadcast_type == "BE" || broadcast_type == "UR" ) {
-        std::cout << "Executando Reliable Broadcast\n";
-        reliable_broadcast_run(process_id);
-    } else if (broadcast_type == "AB") {
-        std::cout << "Executando Atomic Broadcast\n";
-        atomic_broadcast_run(process_id);
-    } else {
-        std::cerr << "Tipo de broadcast desconhecido. Escolha 'BE', 'UR' ou 'AB'.\n";
-    }
-}
+void Application::run(int process_id) {
 
-void Application::reliable_broadcast_run(int process_id) {
     std::vector<uint8_t> message1 = { 'H', 'e', 'l', 'l', 'o' };
 
-    if (process_id == 0) {
-        std::cout << "Reliable Broadcast: Enviando mensagem1 -----\n";
+    if (process_id==0) {
+
+        //std::cout << "Broadcasting message1 -----" << std::endl;
+
         int status = comm->broadcast(message1);
-        std::cout << "Status do envio: " << status << "\n";
+
+        std::cout << "Status do envio: " << status << std::endl;
     } else {
-        for (int i = 0; i < 1; i++) {
+        for (int i=0; i<1; i++) {
+            // Wait to receive a message
             Message received = comm->deliver();
-            std::cout << "\nReliable Broadcast: Mensagem recebida de processo " << received.sender_id << ": ";
+            std::cout << "\n\nReceived message from process " << received.sender_id << ": \n";
             for (auto byte : received.content) {
-                std::cout << byte << ' ';
+                std::cout << byte << std::endl;
             }
-            std::cout << "\n";
+            std::cout << std::endl;
         }
     }
 }
-
-void Application::atomic_broadcast_run(int process_id) {
-    std::vector<uint8_t> message1 = { 'A', 't', 'o', 'm', 'i', 'c' };
-
-    if (process_id == 0) {
-        std::cout << "Atomic Broadcast: Enviando mensagem1 -----\n";
-        Message atomic_message(process_id, 1, 1, 0, message1);
-        atomic_broadcast->broadcast(atomic_message);
-    } else {
-        for (int i = 0; i < 1; i++) {
-            Message received = atomic_broadcast->deliver();
-            std::cout << "\nAtomic Broadcast: Mensagem recebida de processo " << received.sender_id << ": ";
-            for (auto byte : received.content) {
-                std::cout << byte << ' ';
-            }
-            std::cout << "\n";
-        }
-    }
-}
-
