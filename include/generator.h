f505a7bcc2c36a8df607313bb74181880ac6cac2
Luiz Maurício <2003lmvp@gmail.com>
2024-12-09 02:36:35 -0300

    generator should be ready

---
+++ include/generator.h
@@ -0,0 +1,28 @@
+#ifndef GENERATOR_H
+#define GENERATOR_H
+
+#include <vector>
+#include <thread>
+#include "atomic_broadcast_ring.h"
+#include "request.h"
+
+class generator
+{
+private:
+    /* data */
+    std::vector<std::thread> generator_threads;
+    std::queue<Request> message_queue;
+    int write_chance;
+    int total_ops;
+    ReliableComm* comm;
+    std::condition_variable cv_send;
+    std::mutex mtx;
+    int bd_id;
+public:
+    generator(int num_threads, int write_chance, int total_ops, ReliableComm* comm);
+    ~generator();
+    void generate_message();
+    void send_message();
+};
+
+#endif
